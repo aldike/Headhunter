f403f1b9dbcfdc7df85de6978c0901ec5997152a
aldike <aldy_mon@mail.ru>
2023-08-11 03:03:14 +0600

    Certificate added

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 module.exports = {
     development: {
         username: 'admin',
@@ -13,5 +14,10 @@ module.exports = {
         host: 'db-postgresql-fra1-08507-do-user-14504070-0.b.db.ondigitalocean.com',
         dialect: 'postgres',
         port: 25060,
+        dialectOptions: {
+            ssl: {
+              ca: fs.readFileSync('config/ca-certificate.crt')
+            },
+        },
     },
 };
